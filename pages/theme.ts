85d78743de26f95a01ab0b17496a055841d4a2eb
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-10-04 14:55:59 +0200

    Update CSS

--- pages/theme.ts
+++ pages/theme.ts
@@ -6,6 +6,8 @@ export const theme: Theme = extendTheme({
       palette: {
         primary: {
           solidBg: "#D2883D",
+          outlinedBorder: "#D2883D",
+          outlinedColor: "#D2883D",
         },
       },
     },
@@ -24,6 +26,14 @@ export const theme: Theme = extendTheme({
         root: {
           width: "100%",
           maxWidth: "500px",
+        },
+      },
+    },
+    JoyCard: {
+      styleOverrides: {
+        root: {
+          boxShadow:
+            "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;",
         },
       },
     },
