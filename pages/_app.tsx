5979f5a88388c21b933728e2cf1af3b0b425e04b
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-10-06 13:02:06 +0200

    feat(app): add custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@
 /*                                   IMPORTS                                  */
 /* -------------------------------------------------------------------------- */
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { CssVarsProvider } from "@mui/joy/styles";
 import { Layout } from "../components/index";
 import { theme } from "../theme";
@@ -14,18 +13,6 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CssVarsProvider theme={theme}>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400&family=Yeseva+One&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
