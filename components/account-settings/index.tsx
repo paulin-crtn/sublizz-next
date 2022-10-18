cbf3708914d5748678376d61b6ece159149039f8
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-10-19 00:13:14 +0200

    feat(AccountSettings)

--- components/account-settings/index.tsx
+++ components/account-settings/index.tsx
@@ -10,27 +10,27 @@ import Button from "@mui/joy/Button";
 import Alert from "@mui/joy/Alert";
 import ErrorIcon from "@mui/icons-material/Error";
 import CircularProgress from "@mui/joy/CircularProgress";
-import Textarea from "@mui/joy/Textarea";
 import FormHelperText from "@mui/joy/FormHelperText";
 import { updateUser } from "../../utils/featchUser";
 import { useAuth } from "../../context/auth.context";
 import { useAlert } from "../../context/alert.context";
 import { IUser } from "../../interfaces/IUser";
 import { IUpdateUser } from "../../interfaces/IUpdateUser";
-import Chip from "@mui/joy/Chip";
 
 /* -------------------------------------------------------------------------- */
 /*                               REACT COMPONENT                              */
 /* -------------------------------------------------------------------------- */
-const EditProfile = ({ user }: { user: IUser }) => {
+const AccountSettings = ({ user }: { user: IUser }) => {
   /* --------------------------------- CONTEXT -------------------------------- */
   const { setUser } = useAuth();
   const { success } = useAlert();
 
   /* -------------------------------- USE FORM -------------------------------- */
-  const { register, handleSubmit, formState } = useForm<IUpdateUser>({
-    mode: "onTouched",
-  });
+  const { register, handleSubmit, formState, getValues } = useForm<IUpdateUser>(
+    {
+      mode: "onTouched",
+    }
+  );
   const { errors } = formState;
 
   /* ------------------------------ USE MUTATION ------------------------------ */
@@ -39,7 +39,7 @@ const EditProfile = ({ user }: { user: IUser }) => {
     {
       onSuccess: async (data) => {
         setUser(data);
-        success("Profil mis à jour");
+        success("Informations mises à jour");
       },
     }
   );
@@ -54,9 +54,8 @@ const EditProfile = ({ user }: { user: IUser }) => {
     <form onSubmit={handleSubmit(onSubmit)}>
       {isError &&
         error instanceof Error &&
-        error.message.split(",").map((msg, index) => (
+        error.message.split(",").map((msg) => (
           <Alert
-            key={index}
             startDecorator={<ErrorIcon />}
             variant="soft"
             color="danger"
@@ -66,84 +65,44 @@ const EditProfile = ({ user }: { user: IUser }) => {
           </Alert>
         ))}
 
-      <FormControl error={!!errors.firstName}>
-        <FormLabel>Prénom</FormLabel>
+      <FormControl error={!!errors.email}>
+        <FormLabel>Email</FormLabel>
         <Input
-          type="text"
+          type="email"
           variant="soft"
-          defaultValue={user.firstName}
-          {...register("firstName", {
+          defaultValue={user.email}
+          {...register("email", {
             required: "Ce champs est requis",
-            minLength: {
-              value: 3,
-              message: "3 caractères minimum",
-            },
-            maxLength: {
-              value: 30,
-              message: "30 caractères maximum",
+            pattern: {
+              value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+              message: "Vous devez renseigner une adresse email valide",
             },
           })}
         />
-        {errors.firstName && (
-          <FormHelperText>{errors.firstName.message}</FormHelperText>
+        {errors.email && (
+          <FormHelperText>{errors.email.message}</FormHelperText>
         )}
       </FormControl>
 
-      <FormControl error={!!errors.lastName}>
-        <FormLabel>
-          Nom
-          <Chip
-            size="sm"
-            color="info"
-            variant="soft"
-            sx={{ marginLeft: 1, fontWeight: 400 }}
-          >
-            Optionnel
-          </Chip>
-        </FormLabel>
+      <FormControl error={!!errors.password}>
+        <FormLabel>Nouveau mot de passe</FormLabel>
         <Input
-          type="text"
+          type="password"
           variant="soft"
-          defaultValue={user.lastName}
-          {...register("lastName", {
+          {...register("password", {
             minLength: {
-              value: 3,
-              message: "3 caractères minimum",
+              value: 8,
+              message: "8 caractères minimum",
             },
             maxLength: {
-              value: 30,
-              message: "30 caractères maximum",
+              value: 20,
+              message: "20 caractères maximum",
             },
           })}
         />
-        {errors.lastName && (
-          <FormHelperText>{errors.lastName.message}</FormHelperText>
+        {errors.password && (
+          <FormHelperText>{errors.password.message}</FormHelperText>
         )}
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>
-          Message type
-          <Chip
-            size="sm"
-            color="info"
-            variant="soft"
-            sx={{ marginLeft: 1, fontWeight: 400 }}
-          >
-            Optionnel
-          </Chip>
-        </FormLabel>
-        <FormHelperText sx={{ marginTop: "-5px", marginBottom: "10px" }}>
-          Répondez aux annonces plus rapidement en rédigeant un modèle
-          prédéfini.
-        </FormHelperText>
-        <Textarea
-          variant="soft"
-          defaultValue={user.standardMessage}
-          {...register("standardMessage")}
-          minRows={5}
-          maxRows={5}
-        />
       </FormControl>
 
       {!isLoading && <Button type="submit">Enregistrer</Button>}
@@ -156,4 +115,4 @@ const EditProfile = ({ user }: { user: IUser }) => {
   );
 };
 
-export default EditProfile;
+export default AccountSettings;
--- pages/user/account.tsx
+++ pages/user/account.tsx
@@ -2,9 +2,11 @@
 /*                                   IMPORTS                                  */
 /* -------------------------------------------------------------------------- */
 import { NextPage } from "next";
+import Box from "@mui/joy/Box";
 import { useAuth } from "../../context/auth.context";
 import AccountLayout from "../../components/account-layout";
 import AccessDenied from "../../components/access-denied";
+import AccountSettings from "../../components/account-settings";
 
 /* -------------------------------------------------------------------------- */
 /*                               REACT COMPONENT                              */
@@ -17,7 +19,13 @@ const UserAccount: NextPage = () => {
   if (!user) {
     return <AccessDenied />;
   }
-  return <AccountLayout title="Compte">Email</AccountLayout>;
+  return (
+    <AccountLayout title="Profil">
+      <Box sx={{ width: "65%" }}>
+        <AccountSettings user={user} />
+      </Box>
+    </AccountLayout>
+  );
 };
 
 export default UserAccount;
