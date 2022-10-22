893533c5f0679d61e8c9f3364a4ad54d5891a456
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-10-22 02:02:11 +0200

    fix(api): send json response

--- pages/api/supabase/upload-user-profile-picture.ts
+++ pages/api/supabase/upload-user-profile-picture.ts
@@ -25,9 +25,14 @@ const SUPABASE_ANON_API_KEY = process.env.SUPABASE_ANON_API_KEY;
 const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 /* -------------------------------------------------------------------------- */
-/*                                  FUNCTION                                  */
+/*                                API ENDPOINT                                */
 /* -------------------------------------------------------------------------- */
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<{
+  path: string;
+}> {
   // Check that Supabase keys are provided
   if (!SUPABASE_ANON_API_KEY || !NEXT_PUBLIC_SUPABASE_URL) {
     throw new Error("You must provide SUPABASE keys in .env file");
@@ -38,31 +43,43 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     SUPABASE_ANON_API_KEY
   );
   // Parse request with Formidable package
-  const form = formidable({ multiples: false });
-  return form.parse(
-    req,
-    async (error, fields: formidable.Fields, files: formidable.Files) => {
-      if (error) {
-        throw new Error("An error occured while parsing the request");
-      }
-      if (!fields.fileName || !files.profilePicture) {
-        throw new Error("No fileName or profilePicture found");
-      }
-      // Rebuild file
-      const persistentFile = (files.profilePicture as any[])[0];
-      const file = fs.readFileSync(persistentFile.filepath);
-      const fileName = fields.fileName[0];
-      // Upload file to Supabase
-      const response = await supabase.storage
-        .from("user-profile-picture")
-        .upload(fileName, file, {
-          upsert: true,
-          contentType: "image/jpeg",
-        });
-      if (response.error) {
-        throw new Error(response.error.message);
-      }
-      return res.send(response.data);
-    }
-  );
+  const { fields, files } = await _formidableParse(req);
+  // Rebuild file
+  const persistentFile = (files.profilePicture as any[])[0];
+  const file = fs.readFileSync(persistentFile.filepath);
+  const fileName = fields.fileName[0];
+  // Upload file to Supabase
+  const response = await supabase.storage
+    .from("user-profile-picture")
+    .upload(fileName, file, {
+      upsert: true,
+      contentType: "image/jpeg",
+    });
+  if (response.error) {
+    throw new Error(response.error.message);
+  }
+  return res.json(response.data) as unknown as { path: string };
 }
+
+/* -------------------------------------------------------------------------- */
+/*                              PRIVATE FUNCTIONS                             */
+/* -------------------------------------------------------------------------- */
+const _formidableParse = async (
+  req: NextApiRequest
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  return await new Promise(function (resolve) {
+    const form = formidable({ multiples: false });
+    form.parse(
+      req,
+      async (error, fields: formidable.Fields, files: formidable.Files) => {
+        if (error) {
+          throw new Error("An error occured while parsing the request");
+        }
+        resolve({
+          fields,
+          files,
+        });
+      }
+    );
+  });
+};
