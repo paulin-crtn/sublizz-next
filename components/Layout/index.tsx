29217d08d69e601d032f6568fa952bfad11a3b50
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-11-02 11:33:11 +0100

    feat(layout): footer

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,21 +1,37 @@
 /* -------------------------------------------------------------------------- */
 /*                                   IMPORTS                                  */
 /* -------------------------------------------------------------------------- */
+import { Box } from "@mui/joy";
 import { PropsWithChildren } from "react";
 import Footer from "../footer";
 import Navbar from "../navbar";
-import styles from "./layout.module.css";
 
 /* -------------------------------------------------------------------------- */
 /*                               REACT COMPONENT                              */
 /* -------------------------------------------------------------------------- */
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
       <Navbar />
-      <div className={styles.container}>{children}</div>
+      <Box sx={{ flex: "1 0 auto" }}>
+        <Box
+          sx={{
+            maxWidth: "1300px",
+            margin: "0 auto",
+            padding: "45px 30px 90px 30px",
+          }}
+        >
+          {children}
+        </Box>
+      </Box>
       <Footer />
-    </>
+    </Box>
   );
 };
 
