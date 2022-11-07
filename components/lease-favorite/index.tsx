b991906827564ceaa479f0cba66c77e3879c49b6
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-11-07 12:15:14 +0100

    feat(LeaseFavorite): css

--- components/lease-favorite/index.tsx
+++ components/lease-favorite/index.tsx
@@ -11,10 +11,11 @@ import CardContent from "@mui/joy/CardContent";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Typography from "@mui/joy/Typography";
 import Chip from "@mui/joy/Chip";
-import Sheet from "@mui/joy/Sheet";
 import LeaseChips from "../lease-chips";
 import Box from "@mui/joy/Box";
 import Button from "@mui/joy/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import LabelIcon from "@mui/icons-material/Label";
 import noLeaseImg from "../../public/img/no-lease-img.png";
 import { IFavorite } from "../../interfaces/IFavorite";
 import { LEASE_IMAGE_PATH } from "../../const/supabasePath";
@@ -50,51 +51,50 @@ const LeaseFavorite: FunctionComponent<{ leaseFavorite: IFavorite }> = ({
         </AspectRatio>
       </CardOverflow>
 
-      <CardContent sx={{ pl: 3 }}>
-        <Sheet
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+      <CardContent sx={{ display: "flex", flexDirection: "row", pl: 3 }}>
+        <Box sx={{ flex: "1 1" }}>
           <Typography level="h5" fontWeight="600">
             {leaseFavorite.lease.city}
           </Typography>
-          <Typography fontWeight="400">
-            {leaseFavorite.lease.pricePerMonth}€ CC
-          </Typography>
-        </Sheet>
-
-        <Sheet sx={{ display: "flex", alignItems: "center", mt: 0.5, mb: 2 }}>
-          <Box>
-            {!leaseFavorite.lease.endDate && (
-              <Typography level="body1" fontWeight={300}>
-                À partir du{" "}
-                {format(new Date(leaseFavorite.lease.startDate), "dd MMM uuuu")}
-              </Typography>
-            )}
-            {leaseFavorite.lease.endDate && (
-              <Typography level="body1" fontWeight={300}>
-                Du{" "}
-                {format(new Date(leaseFavorite.lease.startDate), "dd MMM uuuu")}{" "}
-                au{" "}
-                {format(new Date(leaseFavorite.lease.endDate), "dd MMM uuuu")}
-              </Typography>
+          <Box display="flex" sx={{ mt: 0.5, mb: 2 }}>
+            <Box>
+              {!leaseFavorite.lease.endDate && (
+                <Typography level="body1" fontWeight={300}>
+                  À partir du{" "}
+                  {format(
+                    new Date(leaseFavorite.lease.startDate),
+                    "dd MMM uuuu"
+                  )}
+                </Typography>
+              )}
+              {leaseFavorite.lease.endDate && (
+                <Typography level="body1" fontWeight={300}>
+                  Du{" "}
+                  {format(
+                    new Date(leaseFavorite.lease.startDate),
+                    "dd MMM uuuu"
+                  )}{" "}
+                  au{" "}
+                  {format(new Date(leaseFavorite.lease.endDate), "dd MMM uuuu")}
+                </Typography>
+              )}
+            </Box>
+            {!!leaseFavorite.lease.isDateFlexible && (
+              <Chip variant="soft" color="neutral" size="sm" sx={{ ml: 1 }}>
+                Dates flexibles
+              </Chip>
             )}
           </Box>
-          {!!leaseFavorite.lease.isDateFlexible && (
-            <Chip variant="soft" color="neutral" size="sm" sx={{ ml: 1 }}>
-              Dates flexibles
-            </Chip>
-          )}
-        </Sheet>
+          <LeaseChips lease={leaseFavorite.lease} size="sm" />
 
-        <LeaseChips lease={leaseFavorite.lease} size="sm" />
+          <Typography level="h6" fontWeight="300" marginTop={2}>
+            {leaseFavorite.lease.pricePerMonth}€ CC
+          </Typography>
+        </Box>
 
-        <Box marginTop={2.5}>
+        <Box sx={{ flex: "0 0 200px" }}>
           <Link href={"/leases/" + leaseFavorite.lease.id}>
-            <Button size="sm" variant="soft" sx={{ marginRight: 1 }}>
+            <Button size="sm" variant="soft" fullWidth sx={{ marginBottom: 1 }}>
               Voir l'annonce
             </Button>
           </Link>
@@ -102,6 +102,8 @@ const LeaseFavorite: FunctionComponent<{ leaseFavorite: IFavorite }> = ({
             size="sm"
             variant="outlined"
             color="neutral"
+            fullWidth
+            startDecorator={<DeleteIcon />}
             onClick={() => removeFavorite(leaseFavorite.id)}
           >
             Retirer des favoris
