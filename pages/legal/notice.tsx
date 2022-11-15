dd2923f21c8daa0ff48601f9e035c3a5933f13cd
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-11-15 16:20:14 +0100

    feat(legal): Privacy

--- pages/legal/notice.tsx
+++ pages/legal/notice.tsx
@@ -6,11 +6,7 @@ import { NextPage } from "next/types";
 import Head from "next/head";
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
-import List from "@mui/joy/List";
-import ListItem from "@mui/joy/ListItem";
 import Button from "@mui/joy/Button";
-import Link from "next/link";
-import Alert from "@mui/joy/Alert";
 import Card from "@mui/joy/Card";
 
 /* -------------------------------------------------------------------------- */
@@ -28,132 +24,104 @@ const NoticePage: NextPage = () => {
         />
       </Head>
 
-      <Box component="main">
-        <Box display="flex" gap={6} position="relative">
-          <Box component="section" flex="1 1">
-            <Typography component="h1" level="h2">
-              Mentions légales
-            </Typography>
-            <Typography marginTop={4}>
-              Conformément aux dispositions des articles 6-III et 19 de la Loi
-              n° 2004-575 du 21 juin 2004 pour la Confiance dans l’économie
-              numérique, dite L.C.E.N., nous portons à la connaissance des
-              utilisateurs et visiteurs du site lacartedeslogements.com les
-              informations suivantes.
-            </Typography>
+      <Box component="main" width="85%">
+        <Typography component="h1" level="h2">
+          Mentions légales
+        </Typography>
+        <Typography marginTop={4}>
+          Conformément aux dispositions des articles 6-III et 19 de la Loi n°
+          2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique,
+          dite L.C.E.N., nous portons à la connaissance des utilisateurs et
+          visiteurs du site lacartedeslogements.com les informations suivantes.
+        </Typography>
 
-            <Typography id="editor" component="h2" level="h4" marginTop={4}>
-              1. Éditeur
-            </Typography>
-            <Typography marginTop={1}>
-              Le site lacartedeslogements.com est la propriété exclusive de
-              Paulin CARATINI, entrepreneur individuel agissant également sous
-              le nom commercial « Haftwald », qui l’édite.
-            </Typography>
-            <Typography marginTop={1}>
-              Haftwald est une micro-entreprise immatriculée sous le numéro
-              SIRET 819 156 910 00021.
-            </Typography>
-            <Button sx={{ mt: 2 }}>Contacter l’éditeur</Button>
+        <Typography id="editor" component="h2" level="h4" marginTop={4}>
+          Éditeur
+        </Typography>
+        <Typography marginTop={2}>
+          Le site lacartedeslogements.com est la propriété exclusive de Paulin
+          CARATINI, entrepreneur individuel agissant également sous le nom
+          commercial « Haftwald », qui l’édite.
+        </Typography>
+        <Typography marginTop={2}>
+          Haftwald est une micro-entreprise immatriculée sous le numéro SIRET
+          819 156 910 00021.
+        </Typography>
+        <Button sx={{ mt: 2 }}>Contacter l’éditeur</Button>
 
-            <Typography id="host" component="h2" level="h4" marginTop={4}>
-              2. Hébergeurs
-            </Typography>
-            <Typography marginTop={1}>
-              Le site lacartedeslogements.com est hébergé sur différents
-              services.
-            </Typography>
+        <Typography id="host" component="h2" level="h4" marginTop={4}>
+          Hébergeurs
+        </Typography>
+        <Typography marginTop={2}>
+          Le site lacartedeslogements.com est hébergé sur différents services.
+        </Typography>
 
-            <Box display="flex" gap={2} marginTop={2}>
-              <Card
-                variant="outlined"
-                sx={{ flex: "1 1 150px", boxShadow: "none" }}
-              >
-                <Typography level="h6">Vercel</Typography>
-                <Typography level="body2">Front-end</Typography>
-                <Typography marginTop={1}>
-                  340 S Lemon Ave #4133
-                  <br />
-                  Walnut, CA 91789
-                  <br />
-                  United States
-                </Typography>
-                <a
-                  href="https://vercel.com/contact"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button sx={{ mt: 2 }}>Contacter Vercel</Button>
-                </a>
-              </Card>
-              <Card
-                variant="outlined"
-                sx={{ flex: "1 1 150px", boxShadow: "none" }}
-              >
-                <Typography level="h6">Heroku</Typography>
-                <Typography level="body2">Back-end</Typography>
-                <Typography marginTop={1}>
-                  415 Mission Street
-                  <br />
-                  Suite 300
-                  <br />
-                  San Francisco, CA 94105
-                  <br />
-                  United States
-                </Typography>
-                <a
-                  href="https://www.heroku.com/contact"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button sx={{ mt: 2 }}>Contacter Heroku</Button>
-                </a>
-              </Card>
-              <Card
-                variant="outlined"
-                sx={{ flex: "1 1 150px", boxShadow: "none" }}
-              >
-                <Typography level="h6">Supabase</Typography>
-                <Typography level="body2">Database & storage</Typography>
-                <Typography marginTop={1}>
-                  970 Toa Payoh North #07-04
-                  <br />
-                  Singapore, Central Singapore
-                  <br />
-                  318992, Singapore
-                </Typography>
-                <a
-                  href="https://supabase.com/support"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Button sx={{ mt: 2 }}>Contacter Supabase</Button>
-                </a>
-              </Card>
-            </Box>
-          </Box>
-
-          <Alert
-            sx={{
-              flex: "0 0 300px",
-              alignSelf: "flex-start",
-              position: "sticky",
-              top: "120px",
-            }}
+        <Box display="flex" gap={2} marginTop={2}>
+          <Card
+            variant="outlined"
+            sx={{ flex: "1 1 150px", boxShadow: "none" }}
           >
-            <List
-              component="ol"
-              aria-label="basic-list"
-              sx={{ fontWeight: 500 }}
+            <Typography level="h6">Vercel</Typography>
+            <Typography level="body2">Front-end</Typography>
+            <Typography marginTop={2}>
+              340 S Lemon Ave #4133
+              <br />
+              Walnut, CA 91789
+              <br />
+              United States
+            </Typography>
+            <a
+              href="https://vercel.com/contact"
+              target="_blank"
+              rel="noreferrer"
             >
-              <ListItem>
-                <Link href="#editor">Éditeur</Link>
-              </ListItem>
-              <ListItem>
-                <Link href="#host">Hébergeurs</Link>
-              </ListItem>
-            </List>
-          </Alert>
+              <Button sx={{ mt: 2 }}>Contacter Vercel</Button>
+            </a>
+          </Card>
+          <Card
+            variant="outlined"
+            sx={{ flex: "1 1 150px", boxShadow: "none" }}
+          >
+            <Typography level="h6">Heroku</Typography>
+            <Typography level="body2">Back-end</Typography>
+            <Typography marginTop={2}>
+              415 Mission Street
+              <br />
+              Suite 300
+              <br />
+              San Francisco, CA 94105
+              <br />
+              United States
+            </Typography>
+            <a
+              href="https://www.heroku.com/contact"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button sx={{ mt: 2 }}>Contacter Heroku</Button>
+            </a>
+          </Card>
+          <Card
+            variant="outlined"
+            sx={{ flex: "1 1 150px", boxShadow: "none" }}
+          >
+            <Typography level="h6">Supabase</Typography>
+            <Typography level="body2">Database & storage</Typography>
+            <Typography marginTop={2}>
+              970 Toa Payoh North #07-04
+              <br />
+              Singapore, Central Singapore
+              <br />
+              318992, Singapore
+            </Typography>
+            <a
+              href="https://supabase.com/support"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button sx={{ mt: 2 }}>Contacter Supabase</Button>
+            </a>
+          </Card>
         </Box>
       </Box>
     </>
--- pages/legal/privacy-and-cookies.tsx
+++ pages/legal/privacy-and-cookies.tsx
@@ -4,6 +4,12 @@
 /* ----------------------------------- NPM ---------------------------------- */
 import type { NextPage } from "next";
 import Head from "next/head";
+import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
+import Button from "@mui/joy/Button";
+import List from "@mui/joy/List";
+import ListItem from "@mui/joy/ListItem";
+import Link from "next/link";
 
 /* -------------------------------------------------------------------------- */
 /*                               REACT COMPONENT                              */
@@ -14,7 +20,7 @@ const PrivacyPage: NextPage = () => {
   /* -------------------------------------------------------------------------- */
 
   return (
-    <div>
+    <>
       <Head>
         <title>
           Politique de confidentialité & Cookies | lacartedeslogements
@@ -24,153 +30,180 @@ const PrivacyPage: NextPage = () => {
           content="Politique de confidentialité et des cookies du site la carte des logements. Annonces de location et de sous-locations de particulier à particulier."
         />
       </Head>
-      <main>
-        <div>
-          <h1>Politique de confidentialité & Cookies</h1>
-        </div>
-        <div>
-          <div>
-            <nav>
-              <ol>
-                <li>
-                  <a href="#data">Données personnelles</a>
-                </li>
-                <li>
-                  <a href="#cookie">Cookies</a>
-                </li>
-              </ol>
-            </nav>
-            <section>
-              <h2 id="data">1. Données personnelles</h2>
-              <p>
-                Les données personnelles sont notamment protégées par la loi n°
-                78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004,
-                l’article L. 226-13 du Code pénal et la Directive Européenne du
-                24 octobre 1995.
-              </p>
+      <Box component="main" width="85%">
+        <Typography component="h1" level="h2">
+          Politique de confidentialité & Cookies
+        </Typography>
 
-              <p>
-                Sur le site haftwald.com, le propriétaire du site collecte
-                certaines informations personnelles relatives à l’utilisateur
-                afin de donner suite aux demandes de contact, de devis ou autres
-                demandes relatives aux services proposées sur le site
-                haftwald.com.
-              </p>
+        <Typography id="editor" component="h2" level="h4" marginTop={4}>
+          Données personnelles
+        </Typography>
 
-              <p>
-                Les informations nominatives requises sont notamment : le
-                prénom, le nom de famille et l’adresse email de l’utilisateur.
-                Sans ces informations, il sera impossible de donner suite à la
-                demande de contact. L’utilisateur fournit ces informations en
-                toute connaissance de cause, notamment lorsqu’il procède par
-                lui-même à leur saisie.
-              </p>
+        <Typography marginTop={2}>
+          Les données personnelles sont notamment protégées par la loi n° 78-87
+          du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l’article L.
+          226-13 du Code pénal et la Directive Européenne du 24 octobre 1995.
+        </Typography>
 
-              <p>
-                Les données sont conservées pour une durée maximale de deux ans
-                dans une base de données mise à disposition par l’hébergeur du
-                site haftwald.com. L’identité et les coordonnées de l’hébergeur
-                sont disponibles sur la page des mentions légales.
-              </p>
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Données collectées et finalités
+        </Typography>
 
-              <p>
-                Aucune information personnelle de l’utilisateur du site
-                haftwald.com n’est publiée à l’insu de l’utilisateur, échangée,
-                transférée, cédée ou vendue sur un support quelconque à des
-                tiers.
-              </p>
+        <Typography marginTop={2}>
+          Sur le site lacartedeslogements.com, le propriétaire du site collecte
+          certaines informations personnelles relatives à l’utilisateur afin de
+          pouvoir procéder à l'inscription de ce dernier et lui faire bénéficier
+          des services proposés sur le site lacartedeslogements.com.
+        </Typography>
+        <Typography marginTop={2}>
+          Les informations nominatives requises sont notamment : le prénom et
+          l’adresse email de l’utilisateur. Sans ces informations, il sera
+          impossible de donner suite à l'inscription.
+        </Typography>
+        <Typography marginTop={2} fontWeight={500}>
+          L’utilisateur fournit ces informations en toute connaissance de cause,
+          notamment lorsqu’il procède par lui-même à leur saisie.
+        </Typography>
 
-              <p>
-                Conformément aux dispositions des articles 38 et suivants de la
-                loi 78-17 du 6 janvier 1978 relative à l’informatique, aux
-                fichiers et aux libertés, tout utilisateur dispose d’un droit
-                d’accès, de rectification, de suppression et d’opposition aux
-                données personnelles le concernant. Pour l’exercer,
-                l’utilisateur doit contacter l’éditeur du site dont les
-                coordonnées sont disponibles sur la page des mentions légales.
-              </p>
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Durée et lieux de conservation des données
+        </Typography>
 
-              <h2 id="cookie">2. Cookies</h2>
+        <Typography marginTop={2}>
+          Les données sont conservées aussi longtemps que l'utilisateur conserve
+          son compte actif. Lorsque le compte est supprimée, les données de
+          l'utilisateur le sont également. Les données sont enregistrées dans
+          une base de données mise à disposition par l'entreprise Supabase. Les
+          coordonnées de cette entreprise sont disponibles sur la page des
+          mentions légales.
+        </Typography>
 
-              <p>
-                L’utilisateur est informé que lors de ses visites sur le site
-                haftwald.com, un ou des cookies sont susceptible de s’installer
-                automatiquement sur son ordinateur, sa tablette ou son
-                smartphone.
-              </p>
+        <Link href="/legal/notice">
+          <Button sx={{ mt: 2 }}>Mentions légales</Button>
+        </Link>
 
-              <p>
-                Un cookie est un fichier de petite taille, qui ne permet pas
-                l’identification de l’utilisateur, mais qui enregistre des
-                informations relatives à la navigation d’un ordinateur sur un
-                site. Les données ainsi obtenues visent à faciliter la
-                navigation ultérieure sur le site, et ont également vocation à
-                permettre diverses mesures de fréquentation.
-              </p>
+        <Typography marginTop={2} fontWeight={500}>
+          Aucune information personnelle de l’utilisateur du site
+          lacartedeslogements.com n’est publiée à l’insu de l’utilisateur,
+          échangée, transférée, cédée ou vendue sur un support quelconque à des
+          tiers. Seul les administrateurs du site lacartedeslogements.com et les
+          hébergeurs du site lacartedeslogements.com peuvent avoir accès aux
+          informations des utilisateurs.
+        </Typography>
 
-              <p>
-                Le site haftwald.com utilise des cookies analytiques. Ces
-                cookies permettent de recueillir des données relatives à votre
-                utilisation du site haftwald.com afin d’améliorer la performance
-                et la conception de celui-ci. Ces cookies peuvent être fournis
-                par un fournisseur d’outil analytique de tierce partie, mais ne
-                sont utilisés qu’à des fins liées au site haftwald.com.
-              </p>
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Délégué à la protection des données
+        </Typography>
 
-              <p>
-                Le paramétrage du logiciel de navigation permet d’informer de la
-                présence de cookie et éventuellement, de le refuser de la
-                manière suivante :
-              </p>
+        <Typography marginTop={2}>
+          Conformément aux dispositions des articles 38 et suivants de la loi
+          78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux
+          libertés, tout utilisateur dispose d’un droit d’accès, de
+          rectification, de suppression et d’opposition aux données personnelles
+          le concernant. Pour l’exercer, l’utilisateur doit contacter l’éditeur
+          du site dont les coordonnées sont disponibles sur la page des mentions
+          légales.
+        </Typography>
 
-              <ul>
-                <li>
-                  <a
-                    href="https://support.google.com/chrome/answer/95647"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Chrome
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://support.mozilla.org/fr/kb/activer-desactiver-cookies"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Firefox
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://support.apple.com/kb/PH17191?viewlocale=fr_FR"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Safari
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://support.microsoft.com/fr-fr/windows/supprimer-et-g%C3%A9rer-les-cookies-168dab11-0753-043d-7c16-ede5947fc64d"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Edge
-                  </a>
-                </li>
-              </ul>
+        <Link href="/legal/notice">
+          <Button sx={{ mt: 2 }}>Mentions légales</Button>
+        </Link>
 
-              <p>
-                Le refus d’installation d’un cookie peut entraîner
-                l’impossibilité d’accéder à certains services. 
-              </p>
-            </section>
-          </div>
-        </div>
-      </main>
-    </div>
+        <Typography id="host" component="h2" level="h4" marginTop={4}>
+          Cookies
+        </Typography>
+
+        <Typography marginTop={2}>
+          L’utilisateur est informé que lors de ses visites sur le site
+          lacartedeslogements.com, un ou des cookies sont susceptibles de
+          s’installer automatiquement sur son ordinateur, sa tablette ou son
+          smartphone.
+        </Typography>
+
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Définition
+        </Typography>
+
+        <Typography marginTop={2}>
+          Un cookie est un fichier de petite taille, qui ne permet pas
+          l’identification de l’utilisateur, mais qui enregistre des
+          informations relatives à la navigation d’un ordinateur sur un site.
+          Les données ainsi obtenues visent à faciliter la navigation ultérieure
+          sur le site, et ont également vocation à permettre diverses mesures de
+          fréquentation.
+        </Typography>
+
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Cookies techniques
+        </Typography>
+
+        <Typography marginTop={2}>
+          Le site lacartedeslogements.com utilise des cookies techniques. Ces
+          cookies sont indispensables au bon fonctionnement du site internet.
+          Exemple de cookie technique : savoir si vous êtes connecté ou non à
+          votre espace client.
+        </Typography>
+
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Cookies analytiques
+        </Typography>
+
+        <Typography marginTop={2}>
+          Le site lacartedeslogements.com utilise des cookies analytiques. Ces
+          cookies permettent de recueillir des données relatives à votre
+          utilisation du site lacartedeslogements.com afin d’améliorer la
+          performance et la conception de celui-ci. Ces cookies peuvent être
+          fournis par un fournisseur d’outil analytique de tierce partie, mais
+          ne sont utilisés qu’à des fins liées au site lacartedeslogements.com.
+        </Typography>
+
+        <Typography id="host" component="h3" level="h5" marginTop={4}>
+          Suppression des cookies
+        </Typography>
+
+        <Typography marginTop={2}>
+          La consulation et le paramétrage du logiciel de navigation permet
+          d’informer de la présence de cookies et éventuellement de les
+          supprimer de la manière suivante :
+        </Typography>
+
+        <Box marginTop={2}>
+          <a
+            href="https://support.google.com/chrome/answer/95647"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button sx={{ mr: 1 }}>Chrome</Button>
+          </a>
+          <a
+            href="https://support.mozilla.org/fr/kb/activer-desactiver-cookies"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button sx={{ mr: 1 }}>Firefox</Button>
+          </a>
+          <a
+            href="https://support.apple.com/kb/PH17191?viewlocale=fr_FR"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button sx={{ mr: 1 }}>Safari</Button>
+          </a>
+          <a
+            href="https://support.microsoft.com/fr-fr/windows/supprimer-et-g%C3%A9rer-les-cookies-168dab11-0753-043d-7c16-ede5947fc64d"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>Edge</Button>
+          </a>
+        </Box>
+
+        <Typography marginTop={2}>
+          Le refus d’installation d’un cookie peut entraîner l’impossibilité
+          d’accéder à certains services.
+        </Typography>
+      </Box>
+    </>
   );
 };
 
