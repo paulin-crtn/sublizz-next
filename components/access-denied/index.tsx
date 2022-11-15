a373ba4aeeb07da94ae7643fac51b88893633418
Paulin Caratini <41628154+paulin-crtn@users.noreply.github.com>
2022-11-15 10:39:23 +0100

    feat(Custom404)

--- components/access-denied/index.tsx
+++ components/access-denied/index.tsx
@@ -9,10 +9,10 @@ import { Box, Typography } from "@mui/joy";
 const AccessDenied = () => {
   /* -------------------------------- TEMPLATE -------------------------------- */
   return (
-    <Box display="flex" alignItems="stretch" minHeight="calc(100vh - 300px)">
+    <Box display="flex" alignItems="stretch" minHeight="calc(100vh - 180px)">
       <Box margin="auto">
-        <Typography component="h1" level="h1" marginBottom={1}>
-          Accès refusé
+        <Typography component="h1" level="h2" marginBottom={1}>
+          L'accès à cette page est restreint
         </Typography>
         <Typography component="h2" level="h4" fontWeight={300}>
           Vous devez vous connecter pour accéder à cette page
---
+++ pages/404.tsx
@@ -0,0 +1,53 @@
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORTS                                  */
+/* -------------------------------------------------------------------------- */
+/* ----------------------------------- NPM ---------------------------------- */
+import { useEffect, useState } from "react";
+import { NextPage } from "next/types";
+import { useRouter } from "next/router";
+import Link from "next/link";
+/* ----------------------------------- MUI ---------------------------------- */
+import Typography from "@mui/joy/Typography";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import SearchIcon from "@mui/icons-material/Search";
+
+/* -------------------------------------------------------------------------- */
+/*                               REACT COMPONENT                              */
+/* -------------------------------------------------------------------------- */
+const Custom404: NextPage = () => {
+  /* --------------------------------- ROUTER --------------------------------- */
+  const router = useRouter();
+
+  /* ------------------------------- REACT STATE ------------------------------ */
+  const [title, setTitle] = useState<string>("La page demandée n'existe pas");
+
+  /* ------------------------------ REACT EFFECT ------------------------------ */
+  useEffect(() => {
+    const pathArr: string[] = router.asPath.split("/");
+    pathArr[1] === "leases" && pathArr.length === 3
+      ? setTitle("L'annonce a été supprimée ou n'existe pas")
+      : setTitle("La page demandée n'existe pas");
+  }, []);
+
+  /* -------------------------------- TEMPLATE -------------------------------- */
+  return (
+    <Box display="flex" alignItems="stretch" minHeight="calc(100vh - 320px)">
+      <Box margin="auto">
+        <Typography component="h1" level="h2" marginBottom={1}>
+          {title}
+        </Typography>
+        <Typography component="h2" level="h4" marginBottom={2} fontWeight={300}>
+          Retrouvez le droit chemin en parcourant nos annonces
+        </Typography>
+        <Link href="/leases">
+          <Button variant="soft" size="lg" startDecorator={<SearchIcon />}>
+            Parcourir les annonces
+          </Button>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default Custom404;
